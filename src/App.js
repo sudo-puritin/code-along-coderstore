5c7fc46121b60853718ab5540733f3dc0a4deb38
Thinh Nguyen D <thinh.coding@gmail.com>
2024-08-21 21:31:33 +0700

    upload files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Link } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
+import ThemeProvider from "./contexts/ThemeProvider";
+import { Typography } from "@mui/material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.coderschool.vn">
+        CoderSchool
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
